80b9e22205467a20dd2071d9abf2346a26b79cc9
Marco Garofalo <marcogarofalo.personal@gmail.com>
2023-12-02 22:18:19 +0000

    Demonstrate waitFor does not work as expected

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,13 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { signal } from '@angular/core';
+import { waitFor } from '@testing-library/angular';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
-  }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+    it("should work but it doesnt", async () => {
+        const target = signal<string[]>([]);
+        setTimeout(() => target.set(["foo", "bar"]), 1000);
+        await waitFor(() => expect(target()).toHaveSize(2), { timeout: 3000 });
+        expect(target()).toEqual(["foo", "bar"]);
+    })
 
-  it(`should have as title 'testing-library-waitfor'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('testing-library-waitfor');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('testing-library-waitfor app is running!');
-  });
 });
